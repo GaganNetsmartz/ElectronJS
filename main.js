16d8517ec5fcf93e3e303269ea19f6172f8f875a
Gagandeep Kaur <gagandeep.kaur@netsmartz.local>
2023-01-04 15:40:08 +0530

    loading a webpage into window browser

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log(`------Hello from Electron------`);
+const { app, BrowserWindow } = require('electron')
+/*  
+    Importing two Electron modules with CommonJS module syntax
+    app: controls application's event lifecycle.
+    BrowserWindow: creates and manages app windows. 
+*/
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  win.loadFile('index.html')
+}
+/*
+The createWindow() function loads web page into a new BrowserWindow instance.
+*/
+
+app.whenReady().then(() => {
+  createWindow()
+})
+// calling function when the app is ready
+
