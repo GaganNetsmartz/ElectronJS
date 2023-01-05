433b930e3d35b3bf729a88656ced6612898c9df6
Gagandeep Kaur <gagandeep.kaur@netsmartz.local>
2023-01-05 21:46:43 +0530

    camera files

--- src/Camera/camera.js
+++ src/Camera/camera.js
@@ -1,3 +1,7 @@
+const video = document.getElementById("camera");
+video.setAttribute('autoplay', '');
+
 navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+    video.srcObject = stream;
     console.log(stream);
 })
